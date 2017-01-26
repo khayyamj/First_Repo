8440f1f0d6f2e2554048969a0305cc69b0e811b9
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-01-26 10:10:04 -0700

    tweak

--- noServer-project/research/js/app.js
+++ noServer-project/research/js/app.js
@@ -9,11 +9,11 @@ var app = angular.module("app", ['ui.router'])
         .state('home', {
             url: '/',
             templateUrl: './views/home/home.html'
-        }),
+        })
         .state('navigation', {
             url: '/nav'
             templateUrl: './views/navigation/navTmpl.html'
-        }),
+        })
         .state('map', {
             url: '/map',
             templateUrl: './views/map/mapTmpl.html'
@@ -21,7 +21,7 @@ var app = angular.module("app", ['ui.router'])
         .state('wrong' {
             url: '/wrong',
             templateUrl: './views/wrongPage/wrongPage.html'
-        })
+        });
 
 
 
